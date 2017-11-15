de64fb05b2e18bf16a40574719e82c046d198359
MALCOLM MEDINA RIASCOS <ASAZING@users.noreply.github.com>
2017-11-15 12:58:53 -0500

    Update name

--- src/project_eae/main.java
+++ src/project_eae/main.java
@@ -8,7 +8,7 @@ public class main {
     public static void main(String[] args) {
         ventana win=new ventana ();
         win.setVisible(true);
-        win.setTitle("Proyecto EAE");
+        win.setTitle("Cajero");
         win.setLocationRelativeTo(null);
         
         
